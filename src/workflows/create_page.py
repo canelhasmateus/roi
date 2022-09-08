12651c62631f3a7283e90b576b2194ffd32572f1
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-09-07 21:26:03 -0300

    sending s3

--- src/workflows/create_page.py
+++ src/workflows/create_page.py
@@ -1,11 +1,44 @@
 import json
+import os
+import pathlib
 import random
 import subprocess
+import time
 import urllib
 import urllib.parse
 
+import boto3 as boto3
+
 from roi_web import PageContent
 from roi_web.processing import load_processed
+
+
+def create_invalidation( distribution_id ):
+    cf = boto3.client( 'cloudfront' )
+    reference = str( time.time() ).replace( ".", "" )
+    res = cf.create_invalidation(
+        DistributionId=distribution_id,
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': 1,
+                'Items': [
+                    '/*'
+                ]
+            },
+            'CallerReference': reference
+        }
+    )
+    invalidation_id = res[ 'Invalidation' ][ 'Id' ]
+    print( f"invalidationId = {invalidation_id}" )
+
+
+def upload_file( file, bucket, object_name ):
+    # Upload the file
+    s3_client = boto3.client( 's3' )
+    response = s3_client.upload_fileobj( file, bucket, object_name,
+                                         ExtraArgs={
+                                             'ContentType': 'text/html'
+                                         } )
 
 
 def asdict( content: PageContent ):
@@ -38,12 +71,21 @@ def main():
     previews = random.choices( queue, k=50 )
     sidebar = random.choices( tool, k=20 )
 
-    with open( r"C:\Users\Mateus\OneDrive\gnosis\akti\src\assets\queue.json", "w" ) as f:
+    akti_distribution = os.environ[ "AKTI_DISTRIBUTION" ]
+    akti_bucket = os.environ[ "AKTI_BUCKET" ]
+    akti_path = pathlib.Path( os.environ[ "AKTI_PATH" ] )
+
+    with open( akti_path / "src/assets/queue.json", "w" ) as f:
         json.dump( previews, f, indent=2 )
-    with open( r"C:\Users\Mateus\OneDrive\gnosis\akti\src\assets\tool.json", "w" ) as f:
+
+    with open( akti_path / "src/assets/tool.json", "w" ) as f:
         json.dump( sidebar, f, indent=2 )
 
-    process = subprocess.run( r"cd C:\Users\Mateus\OneDrive\gnosis\akti && npm run build", shell=True )
+    process = subprocess.run( rf"cd {akti_path} && npm run build", shell=True )
+    with open( akti_path / r"dist/index.html", "rb" ) as dist:
+        upload_file( dist, akti_bucket, "index.html" )
+
+        create_invalidation( akti_distribution )
 
 
 if __name__ == "__main__":
